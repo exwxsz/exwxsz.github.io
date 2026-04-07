c4ebce1e14a981c097b0508f177808f32e04bd54
exwxsz <2581492592@qq.com>
2026-04-08 00:41:52 +0800

    Site updated: 2026-04-08 00:41:51

--- code-flies/数字类型.py
+++ code-flies/数字类型.py
@@ -31,3 +31,41 @@ print(int(-99.321314),'换行\n') # int是直接去掉小数点后面的所有
 print(float('1000323'),float(+2e4),'换行\n') #float()中加单引号是将字符串的数字转换为浮点数，float()中加+号是将数字转换为浮点数，不加单引号就是直接将整数或者其他数字类型转换为浮点数
 print(complex(-1 - 45j),'换行\n','不加空格的字符串形式也就是双引号里面加\n',complex("23-312j")) #complex()中加单引号是将字符串的数字转换为复数，complex()中加+号是将数字转换为复数，不加单引号就是直接将整数或者其他数字类型转换为复数
 print(pow(4,2),'换行\n',2 ** 3,'换行\n',pow(4,2,3),'换行测试\n',4 ** 2 % 3) #pow(x,y)函数返回x的y次幂，pow(x,y,z)会将幂运算的结果对z进行求余的运算
+
+a = eval(input('请输入一个数字：')) #eval()函数将输入的字符串当成有效的表达式来求值，并返回结果
+b = eval(input('请输入另一个数字：')) #input()函数用于获取用户输入的字符串，eval()函数将输入的字符串当成有效的表达式来求值，并返回结果
+c = eval(input('请输入一个数字：')) #input()函数用于获取用户输入的字符串，eval()函数将输入的字符串当成有效的表达式来求值，并返回结果
+d = a+b+c
+e = (a+b+c)/3
+print(f'你输入的三个数字的和是：{d},平均数是：{e:.2f}',sep='') #f-string格式化字符串，使用花括号{}来引用变量，输出结果为你输入的三个数字的和是：d，平均数是：e
+
+todaydate = input('输入今天的日期，格式为xxxx.xx.xx：')
+print(f'今天是{todaydate},我第一次独立写程序啦！',sep=',') #f-string格式化字符串，使用花括号{}来引用变量，输出结果为今天是todaydate，我第一次独立写程序啦！
+
+
+userbay = input('').split('和') #input()函数用于获取用户输入的字符串，split()方法将输入的字符串按照空格分割成一个列表
+float_userbay_1 = float(userbay[0]) #直接用上面split('')进行捕获。split的括号里面跟的就是你要并列的字符，例如我这里是“和”字，那么输入一个数就跟一个“和”字，但不需要空格。如果你输入的数据超过下面变量的内容，那么多出的内容就会没有捕获。它split就相当于把你输入的数据整理成了一个定义在你input前面的变量里面。而且split只能接input用户输入的形式
+float_userbay_2 = float(userbay[1]) #将列表中的字符串转换为浮点数
+float_userbay_3 = float(userbay[2]) 
+all_userbay = float_userbay_1 + float_userbay_2 + float_userbay_3
+
+print(f'应付{all_userbay:.1f}元') #f-string格式化字符串，使用花括号{}来引用变量，输出结果为应付all_userbay元，保留两位小数
+
+
+x_str = input('请输入一个数字：').split(',')#input()函数用于获取用户输入的字符串
+import decimal #引入decimal模块
+x_int_1 = decimal.Decimal(x_str[0])
+x_int_2 = decimal.Decimal(x_str[1])
+print(f'{x_int_1}*{x_int_2}={x_int_1*x_int_2}') #f-string格式化字符串，使用花括号{}来引用变量，输出结果为x_int_1*x_int_2=x_int_1*x_int_2 '''
+
+song_words = "爱我中华,五十六个星座五十六枝花,五十六族兄弟姐妹是一家,五十六种语言汇成一句话,爱我中华爱我中华爱我中华,爱我中华,健儿奋起的步伐,爱我中华,建设我们的国家,爱我中华,中华雄姿英发,爱我中华,五十六族兄弟姐妹,五十六种语言汇成一句话,爱我中华。"
+song_tages = input('请输入要查找的字符串：') 
+song_tages_count = song_words.count(song_tages) #变量.count(变量)的形式来查找内容，并计算次数
+print(f'{song_tages}出现{song_tages_count}次')
+
+user_input_words = input('').lower() #input()函数用于获取用户输入的字符串，lower()方法将输入的字符串转换为小写字母
+user_input_words_long = len(user_input_words) #len()函数返回字符串的长度
+user_input_words_lower = user_input_words.lower() #lower()方法将字符串转换为小写字母
+print(f'{user_input_words_lower[0:5]}') #输出字符串的前6个字符
+
+
